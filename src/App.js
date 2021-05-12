94525568e7dcf42eaa2f10f75271659094f1f33d
yhan0704 <hanyc1205@gmail.com>
2021-05-12 11:51:16 -0400

    gif API show items when user click button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  const URL = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=hf8BCWJfRr3et5BKOyfjvGTtNLESl9Ko&rating=g&limit=3`;
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setGifs(data.data));
+  }, [URL]);
+
+  const searchItems = (e) => {
+    e.preventDefault();
+    setQuery(e.target.childNodes[0].value);
+  };
+
+  console.log(gifs);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="submit" onSubmit={(e) => searchItems(e)}>
+        <input
+          type="text"
+          defaultValue={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button>search</button>
+      </form>
+      <p>{search}</p>
+
+      {gifs.map((gif) => (
+        <div>
+          <p key={gif.id}>{gif.id}</p>
+          <p>
+            <img src={gif.images.downsized.url} alt="pictures" />
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
